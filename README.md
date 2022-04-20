03e831cd249fd28e545c7d97048e4d6b395d1edf
Jonathan Matarazzi <git@jonathanm.nl>
2022-04-20 10:01:19 +0200

    Migration to organisation

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# receiver
+## Python client troubleshooting
+For the bluetooth functionality you will need the PyBluez python package. The current version won't install on Windows 10, included in the python package is a customized version of the PyBluez package that will install. Open a pipenv shell in the pybluez folder and run `python setup.py install` to install the package.
+er
---
+++ connect.py
@@ -0,0 +1,58 @@
+import bluetooth
+import time
+
+devices = bluetooth.discover_devices(lookup_names=True)
+print(devices)
+
+wirelessIMUs = []
+for device in devices:
+    if device[1] == 'WirelessIMU':
+        wirelessIMUs.append(device)
+
+print(wirelessIMUs)
+
+IMUservices = []
+
+for addr, name in wirelessIMUs:
+    print(addr)
+    services = bluetooth.find_service(address=addr)
+    print(services)
+    for serv in services:
+        if serv['name'] == b'ESP32SPP\x00':
+            IMUservices.append(serv)
+
+print(IMUservices)
+
+sensors = []
+
+for IMU in IMUservices:
+    sensor = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+    sensor.connect((IMU['host'], IMU['port']))
+    sensor.setblocking(0)
+    sensor.settimeout(1000)
+    sensors.append(sensor)
+
+start = time.time()
+
+output = [None] * 6
+
+for sensor in sensors:
+    sensor.send('a')
+
+for i in range(1000):
+    for sensor in sensors:
+        inbytes = b''
+        while len(inbytes)<12:
+            inbytes+=sensor.recv(12-len(inbytes))
+        sensor.send('a')
+        #input = sensor.recv(12)
+        for j in range(0, 12, 2):
+            output[int(j/2)] = inbytes[j] << 8 | inbytes[j + 1]
+        print(output)
+
+
+
+end = time.time()
+avg = (end - start) / 1000
+f = 1 / avg
+print('frequency: %f' % f)
